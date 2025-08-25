d1b73e98ceee73ab236670fed9a3f8760ce8a38f
suzukisaki <mck.bt.yshr.s@gmail.com>
2025-08-25 00:24:19 -0700

    test

---
+++ js/main.js
@@ -0,0 +1,118 @@
+// 穴埋め形式です。空いている箇所を埋めて実装してください
+
+$(function () {
+  /*=================================================
+  ハンバーガ―メニュー
+  ===================================================*/
+  // ハンバーガーメニューをクリックした時
+  $(".toggle-btn ").on("click", function () {
+    $("header").toggleClass("open");
+  });
+  // メニューのリンクをクリックした時
+  $("nav a").on("click", function () {
+    $("header").removeClass("open")
+  });
+
+
+
+  /*=================================================
+  スクロール時の画像フェード表示
+  ===================================================*/
+  // スクロール時のイベント
+  $(window).scroll(function () {
+    $(".section-title").each(function () {
+      let scroll = $(window).scrollTop();
+      let target = $(this).offset().top;
+      let windowHeight = $(window).height();
+
+      if (scroll > target - windowHeight + 200) {
+        $(this).css({
+          "color": "rgba(141, 156, 184, 1)", // 文字だけ不透明に
+          "transform": "translateY(0)"
+        });
+      }
+    });
+  });
+
+  /*=================================================
+  スムーススクロール
+  ===================================================*/
+  // ページ内リンクのイベント
+  $('a[href^="#"]').click(function () {
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, 600, "swing");
+    return false;
+  });
+
+
+  /*=================================================
+  スリック
+  ===================================================*/
+  $('.slider-text').slick({
+    dots: true,
+    autoplay: true,
+    speed: 1000,
+    arrows: false,
+    asNavFor: '.slider-img'
+  });
+  
+  $('.slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-text'
+  });
+
+  /*=================================================
+  ヘッダーの色をセクションによって変える
+  ===================================================*/
+  $(window).on("scroll", function () {
+    let scroll = $(window).scrollTop();
+
+    $("section").each(function () {
+      let top = $(this).offset().top - 100; // 少し余裕を持たせる
+      let bottom = top + $(this).outerHeight();
+
+      if (scroll >= top && scroll < bottom) {
+        // sectionのidを取得
+        let id = $(this).attr("id");
+
+        // headerにクラスを付け替える
+        $("header").attr("class", id + "-color");
+      }
+    });
+  });
+
+  /*=================================================
+   ページ読み込み完了でローディング非表示
+  ===================================================*/
+  $(window).on('load', function () {
+    $(".loader").delay(1500).fadeOut('slow');
+    $(".loading_txt").delay(1200).fadeOut('slow');
+  });
+
+
+  /*=================================================
+   ふわっと出てくる 
+  ===================================================*/
+
+  $(window).scroll(function () {
+    $(".fadeIn,.fadeIn-right,.fadeIn-left").each(function () {
+      var scroll = $(window).scrollTop();
+      var target = $(this).offset().top;
+      var windowHeight = $(window).height();
+      if (scroll > target - windowHeight + $(this).outerHeight()) {
+        // outerHeight()はpaddingを含めた高さを取得する
+        $(this).addClass("is-show");
+      }
+    });
+  });
+
+});
+
+
+
+
